a9080bb279a4c5471e2a12168f98f7b704d58bd4
farhanf11 <58339566+farhanf11@users.noreply.github.com>
2022-02-11 21:47:06 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+       fontSize : {
+        ls : ['0.5rem', '0.75rem'],
+        ms : ['0.625rem', '1rem']
+      },
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
